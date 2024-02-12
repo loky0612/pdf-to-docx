a01f8c54ae41a4541714ac352f0902bef7e38a65
LOKESH M <115631279+loky0612@users.noreply.github.com>
2024-02-12 18:16:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use('/', require('./routes/authRoutes'));
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.json("Hello")
